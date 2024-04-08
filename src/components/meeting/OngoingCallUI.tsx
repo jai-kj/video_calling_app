42a496c4e4a9033688578ebd6ff11ae6847e22ff
jai-kj <jai.j@somaiya.edu>
2024-04-09 01:20:56 +0530

    fix(call): multiple joins of same user on route switch

--- src/components/meeting/OngoingCallUI.tsx
+++ src/components/meeting/OngoingCallUI.tsx
@@ -1,10 +1,21 @@
+import { useNavigationEvent } from "@/hooks/useNavigationEvent";
+import { useStreamCall } from "@/hooks/useStreamCall";
 import { CallingState, useCallStateHooks } from "@stream-io/video-react-sdk";
 import { Loader2 } from "lucide-react";
+import { useCallback } from "react";
 import CallLayouts from "../layout/CallLayouts";
 
 const OngoingCallUI = () => {
+  const call = useStreamCall();
+
   const { useCallCallingState } = useCallStateHooks();
   const callingState = useCallCallingState();
+
+  const closeCallIfPathChange = useCallback(() => {
+    call.leave();
+  }, [call]);
+
+  useNavigationEvent({ onPathChange: closeCallIfPathChange });
 
   if (callingState !== CallingState.JOINED) {
     return <Loader2 className="mx-auto animate-spin" />;
---
+++ src/hooks/useNavigationEvent.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+interface IUseNavigationEventProps {
+  onPathChange: () => void;
+}
+
+export const useNavigationEvent = ({
+  onPathChange,
+}: IUseNavigationEventProps) => {
+  const pathname = usePathname();
+  let effectCount = 0;
+
+  useEffect(() => {
+    return () => {
+      if (effectCount++ == 1) {
+        onPathChange();
+      }
+    };
+  }, [pathname, onPathChange, effectCount]);
+};
